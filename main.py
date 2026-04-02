7423529188a7e6128d52038b6b0fc189c6dd9b16
Hongyu Cai <h.tsai@hotmail.com>
2026-04-02 00:17:45 +0000

    /

--- main.py
+++
@@ -1,8 +0,0 @@
-from pathlib import Path
-
-from datasets import load_dataset
-
-cwd = Path().cwd()
-assert cwd.name == "workdir"
-
-dataset = list(load_dataset("walledai/AdvBench")["train"]["prompt"])
